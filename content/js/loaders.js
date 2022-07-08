420a999f965448521c20323b691a712687f3b1f6
Giggi Argento <giggi.argento@gmail.com>
2022-07-07 23:20:19 -0300

    Fix height

--- content/js/loaders.js
+++ content/js/loaders.js
@@ -1,14 +1,14 @@
-// IMPORTANTE: mientras estemos trabajando localmente, estas funciones no van a andar a menos que la página esté corriendo en un server, usar 'Open with Live Server' en Code para verlo
+// IMPORTANTE: mientras estemos trabajando localmente, estas funciones no van a andar a menos que la pï¿½gina estï¿½ corriendo en un server, usar 'Open with Live Server' en Code para verlo
 
-// cuando carga la página, ejecuto la función que trae lo que haya en topNav.html, lo inserta en el div#nav-loader y borra ese div contenedor
+// cuando carga la pï¿½gina, ejecuto la funciï¿½n que trae lo que haya en topNav.html, lo inserta en el div#nav-loader y borra ese div contenedor
 // lo mismo con el footer
 
 $(document).ready(function() {
-    $('#nav-loader').load('../webComponents/topNav.html', function() {
+    $('#navLoader').load('../webComponents/topNav.html', function() {
         $('#topNav').unwrap();
     })
 
-    $('#footer-loader').load('../webComponents/footer.html', function() {
+    $('#footerLoader').load('../webComponents/footer.html', function() {
         $('footer').unwrap();
     })
 })
