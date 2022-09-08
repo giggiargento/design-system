d0272d2ce83bcd121e516cae805df3beafb86217
Giggi <giggi.argento@gmail.com>
2022-09-08 10:42:33 -0300

    adding copyToClipboard function

---
+++ content/js/JavaScript.js
@@ -0,0 +1,15 @@
+//Copy to clipboard
+function copyToClipboard() {
+    // Get the text field
+    var copyText = document.getElementById("myCode");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
