1d05c9f8c7db833a57dc09ce2d78971682aa0957
mechamech <mechamena@gmail.com>
2022-09-08 13:27:58 -0300

    update versión bootstrap, funcionalidad tooltip copy to clipboard

--- content/js/JavaScript.js
+++ content/js/JavaScript.js
@@ -1,4 +1,13 @@
-function copyToClipboard() {
+function copyToClipboard(event) {
   var copyText = document.getElementById("myCode").textContent;
   navigator.clipboard.writeText(copyText);
+  
+  // busco el tooltip de copyBtn
+  const tooltip = bootstrap.Tooltip.getInstance(`#${event.target.id}`)
+  // le cambio el texto
+  tooltip.setContent({ '.tooltip-inner': 'Copied!' })
+  // al ocultarlo, vuelvo a cambiar el texto al original
+  document.getElementById('copyBtn').addEventListener("hidden.bs.tooltip",function () {tooltip.setContent(
+    {".tooltip-inner": 'Copy to clipboard'}
+  )})
 }
