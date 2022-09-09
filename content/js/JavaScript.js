974e4b7195df858938631ec04893e045800f283b
Giggi <giggi.argento@gmail.com>
2022-09-09 16:16:02 -0300

    fixing id

--- content/js/JavaScript.js
+++ content/js/JavaScript.js
@@ -1,13 +1,17 @@
-function copyToClipboard(event) {
-  var copyText = document.getElementById("myCode").textContent;
+function copyToClipboard(codeId) {
+  var copyText = document.getElementById(codeId).textContent;
   navigator.clipboard.writeText(copyText);
-  
   // busco el tooltip de copyBtn
   const tooltip = bootstrap.Tooltip.getInstance(`#${event.target.id}`)
+  console.log(tooltip)
   // le cambio el texto
   tooltip.setContent({ '.tooltip-inner': 'Copied!' })
   // al ocultarlo, vuelvo a cambiar el texto al original
-  document.getElementById('copyBtn').addEventListener("hidden.bs.tooltip",function () {tooltip.setContent(
+
+  //document.getElementById('copyBtn').addEventListener("hidden.bs.tooltip",function () {tooltip.setContent(
+  //  {".tooltip-inner": 'Copy to clipboard'}
+  //)})
+  event.target.addEventListener("hidden.bs.tooltip",function () {tooltip.setContent(
     {".tooltip-inner": 'Copy to clipboard'}
   )})
 }
@@ -19,8 +23,8 @@ function copyToClipboard(event) {
 //  document.getElementById('showHideCode').innerText='Hide code';
 //}
 
-const micodigo = document.querySelector("#myCode");
-const button = document.querySelector("#showHideCode");
+const micodigo = document.querySelector(".myCode");
+const button = document.querySelector(".showHideCode");
 
 button.addEventListener("click", showCode)
 
@@ -37,88 +41,89 @@ function showCode(){
 //codeBox code
 //buttons
 //primaryButtons
-function insertPrimaryButtonCode(){
-  document.getElementById('primaryButtonCode').innerText =
-`<button class="pbtn btn-primary">Label</button>
 
-<button class="pbtn btn-primary">
-<span class="material-icons">arrow_forward</span>Label
-</button>
-
-<button class="pbtn btn-primary btn-icon">
-<span class="material-icons">more_horiz</span>
-</button>
-
-<button class="pbtn btn-primary btn-disabled">Label</button>
-
-<button class="pbtn btn-primary btn-disabled">
-<span class="material-icons">arrow_forward</span>Label
-</button>
-
-<button class="pbtn btn-primary btn-icon btn-disabled">
-<span class="material-icons">more_horiz</span>
-</button>`
-;
-}
-
-window.addEventListener('DOMContentLoaded', function() {
-  insertPrimaryButtonCode();
-}, true);
-
+//function insertPrimaryButtonCode(){
+//  document.getElementById('primaryButtonCode').innerText =
+//`<button class="pbtn btn-primary">Label</button>
+//
+//<button class="pbtn btn-primary">
+//<span class="material-icons">arrow_forward</span>Label
+//</button>
+//
+//<button class="pbtn btn-primary btn-icon">
+//<span class="material-icons">more_horiz</span>
+//</button>
+//
+//<button class="pbtn btn-primary btn-disabled">Label</button>
+//
+//<button class="pbtn btn-primary btn-disabled">
+//<span class="material-icons">arrow_forward</span>Label
+//</button>
+//
+//<button class="pbtn btn-primary btn-icon btn-disabled">
+//<span class="material-icons">more_horiz</span>
+//</button>`
+//;
+//}
+//
+//window.addEventListener('DOMContentLoaded', function() {
+//  insertPrimaryButtonCode();
+//}, true);
+//
 //secondaryButtons
-function insertSecondaryButtonCode(){
-  document.getElementById('secondaryButtonCode').innerText =
-`<button class="pbtn btn-secondary">Label</button>
-
-<button class="pbtn btn-secondary">
-<span class="material-icons">arrow_forward</span>Label
-</button>
-
-<button class="pbtn btn-secondary btn-icon">
-<span class="material-icons">more_horiz</span>
-</button>
-
-<button class="pbtn btn-secondary btn-disabled">Label</button>
-
-<button class="pbtn btn-secondary btn-disabled">
-<span class="material-icons">arrow_forward</span>Label
-</button>
-
-<button class="pbtn btn-secondary btn-icon btn-disabled">
-<span class="material-icons">more_horiz</span>
-</button>`
-;
-}
-
-window.addEventListener('DOMContentLoaded', function() {
-  insertSecondaryButtonCode();
-}, true);
-
-//errorButtons
-function insertErrorButtonCode(){
-  document.getElementById('errorButtonCode').innerText =
-`<button class="pbtn btn-red">Label</button>
-
-<button class="pbtn btn-red">
-<span class="material-icons">delete</span>Label
-</button>
-
-<button class="pbtn btn-red btn-icon">
-<span class="material-icons">delete</span>
-</button>
-
-<button class="pbtn btn-red btn-disabled">Label</button>
-
-<button class="pbtn btn-red btn-disabled">
-<span class="material-icons">delete</span>Label
-</button>
-
-<button class="pbtn btn-red btn-icon btn-disabled">
-<span class="material-icons">delete</span>
-</button>`
-;
-}
-
-window.addEventListener('DOMContentLoaded', function() {
-  insertErrorButtonCode();
-}, true);
+//function insertSecondaryButtonCode(){
+//  document.getElementById('secondaryButtonCode').innerText =
+//`<button class="pbtn btn-secondary">Label</button>
+//
+//<button class="pbtn btn-secondary">
+//<span class="material-icons">arrow_forward</span>Label
+//</button>
+//
+//<button class="pbtn btn-secondary btn-icon">
+//<span class="material-icons">more_horiz</span>
+//</button>
+//
+//<button class="pbtn btn-secondary btn-disabled">Label</button>
+//
+//<button class="pbtn btn-secondary btn-disabled">
+//<span class="material-icons">arrow_forward</span>Label
+//</button>
+//
+//<button class="pbtn btn-secondary btn-icon btn-disabled">
+//<span class="material-icons">more_horiz</span>
+//</button>`
+//;
+//}
+//
+//window.addEventListener('DOMContentLoaded', function() {
+//  insertSecondaryButtonCode();
+//}, true);
+//
+////errorButtons
+//function insertErrorButtonCode(){
+//  document.getElementById('errorButtonCode').innerText =
+//`<button class="pbtn btn-red">Label</button>
+//
+//<button class="pbtn btn-red">
+//<span class="material-icons">delete</span>Label
+//</button>
+//
+//<button class="pbtn btn-red btn-icon">
+//<span class="material-icons">delete</span>
+//</button>
+//
+//<button class="pbtn btn-red btn-disabled">Label</button>
+//
+//<button class="pbtn btn-red btn-disabled">
+//<span class="material-icons">delete</span>Label
+//</button>
+//
+//<button class="pbtn btn-red btn-icon btn-disabled">
+//<span class="material-icons">delete</span>
+//</button>`
+//;
+//}
+//
+//window.addEventListener('DOMContentLoaded', function() {
+//  insertErrorButtonCode();
+//}, true);
