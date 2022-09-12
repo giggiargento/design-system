95518cc536984cd0c75ae34889c8f3a01e447144
Giggi <giggi.argento@gmail.com>
2022-09-12 12:31:19 -0300

    add tooltip to nav items

--- content/js/JavaScript.js
+++ content/js/JavaScript.js
@@ -7,10 +7,6 @@ function copyToClipboard(codeId) {
   // le cambio el texto
   tooltip.setContent({ '.tooltip-inner': 'Copied!' })
   // al ocultarlo, vuelvo a cambiar el texto al original
-
-  //document.getElementById('copyBtn').addEventListener("hidden.bs.tooltip",function () {tooltip.setContent(
-  //  {".tooltip-inner": 'Copy to clipboard'}
-  //)})
   event.target.addEventListener("hidden.bs.tooltip",function () {tooltip.setContent(
     {".tooltip-inner": 'Copy to clipboard'}
   )})
@@ -18,11 +14,6 @@ function copyToClipboard(codeId) {
 
 
 //codeBox
-//change btw show code and hide code
-//function changeHideShowCode(event){
-//  document.getElementById('showHideCode').innerText='Hide code';
-//}
-
 const button = document.querySelectorAll(".showHideCode");
 
 button.forEach(btn => btn.addEventListener("click", showCode))
@@ -37,92 +28,8 @@ function showCode() {
   }
 }
 
-//codeBox code
-//buttons
-//primaryButtons
-
-//function insertPrimaryButtonCode(){
-//  document.getElementById('primaryButtonCode').innerText =
-//`<button class="pbtn btn-primary">Label</button>
-//
-//<button class="pbtn btn-primary">
-//<span class="material-icons">arrow_forward</span>Label
-//</button>
-//
-//<button class="pbtn btn-primary btn-icon">
-//<span class="material-icons">more_horiz</span>
-//</button>
-//
-//<button class="pbtn btn-primary btn-disabled">Label</button>
-//
-//<button class="pbtn btn-primary btn-disabled">
-//<span class="material-icons">arrow_forward</span>Label
-//</button>
-//
-//<button class="pbtn btn-primary btn-icon btn-disabled">
-//<span class="material-icons">more_horiz</span>
-//</button>`
-//;
-//}
-//
-//window.addEventListener('DOMContentLoaded', function() {
-//  insertPrimaryButtonCode();
-//}, true);
-//
-//secondaryButtons
-//function insertSecondaryButtonCode(){
-//  document.getElementById('secondaryButtonCode').innerText =
-//`<button class="pbtn btn-secondary">Label</button>
-//
-//<button class="pbtn btn-secondary">
-//<span class="material-icons">arrow_forward</span>Label
-//</button>
-//
-//<button class="pbtn btn-secondary btn-icon">
-//<span class="material-icons">more_horiz</span>
-//</button>
-//
-//<button class="pbtn btn-secondary btn-disabled">Label</button>
-//
-//<button class="pbtn btn-secondary btn-disabled">
-//<span class="material-icons">arrow_forward</span>Label
-//</button>
-//
-//<button class="pbtn btn-secondary btn-icon btn-disabled">
-//<span class="material-icons">more_horiz</span>
-//</button>`
-//;
-//}
-//
-//window.addEventListener('DOMContentLoaded', function() {
-//  insertSecondaryButtonCode();
-//}, true);
-//
-////errorButtons
-//function insertErrorButtonCode(){
-//  document.getElementById('errorButtonCode').innerText =
-//`<button class="pbtn btn-red">Label</button>
-//
-//<button class="pbtn btn-red">
-//<span class="material-icons">delete</span>Label
-//</button>
-//
-//<button class="pbtn btn-red btn-icon">
-//<span class="material-icons">delete</span>
-//</button>
-//
-//<button class="pbtn btn-red btn-disabled">Label</button>
-//
-//<button class="pbtn btn-red btn-disabled">
-//<span class="material-icons">delete</span>Label
-//</button>
-//
-//<button class="pbtn btn-red btn-icon btn-disabled">
-//<span class="material-icons">delete</span>
-//</button>`
-//;
-//}
-//
-//window.addEventListener('DOMContentLoaded', function() {
-//  insertErrorButtonCode();
-//}, true);
+//Nav links active
+$(".nav-link").on("click", function(){
+	$(".nav-link.active").removeClass("active");
+	$(this).addClass("active");
+});
