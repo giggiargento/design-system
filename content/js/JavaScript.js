a5341536b3deb6e0d147c5f757798ad8f5e0f880
Giggi <giggi.argento@gmail.com>
2022-09-12 16:32:50 -0300

    finished with active sideNav and topNav in all pages

--- content/js/JavaScript.js
+++ content/js/JavaScript.js
@@ -28,13 +28,34 @@ function showCode() {
   }
 }
 
-// guardo esta lista de páginas de componentes para saber cuándo activar el link components en el top nav
-var componentPages = ['buttons', 'calendar', 'cards', 'checkbox', 'dropdowns', 'forms', 'navigation', 'notifications', 'pagination', 'radio', 'select', 'sidesheet', 'switch', 'table', 'tabs', 'tags', 'tooltip'];
+// guardo lista de las secciones de cada para saber cuando activar el link en el top nav
+//Brand
+var brandPages = ['mission', 'personality', 'promise', 'values'];
+//Foundations
+var foundationsPages = ['grid', 'icons', 'typography', 'colors', 'illustrations'];
+//Components
+var componentsPages = ['buttons', 'calendar', 'cards', 'checkbox', 'dropdowns', 'forms', 'navigation', 'notifications', 'pagination', 'radio', 'select', 'sidesheet', 'switch', 'table', 'tabs', 'tags', 'tooltip'];
+//Patterns
+var patternsPages = ['emptyStates', 'forms', 'messages'];
+//Resources
+var resourcesPages = ['introduction', 'download', 'versions'];
 
 function activateTopNavLink() {
   $('#topNav a.active').removeClass('active').removeAttr('aria-current');
-  if (componentPages.find(page => location.pathname.includes(page))) {
+    if (brandPages.find(page => location.pathname.includes(page))) {
+      $('.nav-link.brand').addClass('active')
+  }
+  if (foundationsPages.find(page => location.pathname.includes(page))) {
+    $('.nav-link.foundations').addClass('active')
+  }
+  if (componentsPages.find(page => location.pathname.includes(page))) {
       $('.nav-link.components').addClass('active')
+  }
+  if (patternsPages.find(page => location.pathname.includes(page))) {
+    $('.nav-link.patterns').addClass('active')
+  }
+  if (resourcesPages.find(page => location.pathname.includes(page))) {
+    $('.nav-link.resources').addClass('active')
   }
 
   else {
