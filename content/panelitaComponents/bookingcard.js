2b015e6fca13e92ae0393ef46eb97920323be768
mechamech <mechamena@gmail.com>
2022-10-21 13:40:20 -0300

    cards

--- content/panelitaComponents/bookingcard.js
+++ content/panelitaComponents/bookingcard.js
@@ -9,10 +9,49 @@ function togglePreparation(link) {
   else {
     link.addClass('active');
     link.closest('.booking-card-container').find('.sub-card').removeClass('visible');
-    // link.closest('.booking-card-container').addClass('expanded');
     let target = link.closest('.booking-card').next('.preparation-card')
     if (!target.hasClass('visible')) {
       target.addClass('visible');
     }
+  }
+}
+
+function showSubcard(btn) {
+  btn = $(btn);
+  let action = btn.attr('data-action');
+  let target = btn.closest('.booking-card-container').find(`.sub-card.${action}-card`);
+  if (!target.hasClass('visible')) {
+    $('.sub-card').not(target).removeClass('visible');
+    btn.closest('.booking-card-container').addClass('expanded');
+    target.addClass('visible');
+    animateShowSubcard(target);
+  };
+}
+
+function animateShowSubcard(card) {
+  card[0].scrollIntoView({behavior: 'smooth'});
+  if (card.find('select').length > 0) {
+    $('select').chosen()
+  };
+}
+
+function animateHideSubcard(btn) {
+  btn = $(btn);
+  let target = btn.closest('.sub-card');
+  btn.closest('.booking-card-container').removeClass('expanded');
+  if (target.hasClass('visible')) {
+    target.removeClass('visible');
+  }
+}
+
+function toggleActionsMobile(btn) {
+  btn = $(btn);
+  let target = btn.closest('.booking-card').find('.actions');
+  if (target.hasClass('visible')) {
+    target.removeClass('visible');
+  }
+
+  else {
+    target.addClass('visible');
   }
 }
