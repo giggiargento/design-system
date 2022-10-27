4c3fddb678b8cafcba0a18c9cedef6c71e07f590
mechamech <mechamena@gmail.com>
2022-10-27 13:16:22 -0300

    búsqueda

--- content/js/JavaScript.js
+++ content/js/JavaScript.js
@@ -224,33 +224,64 @@ function navbarTogglerBg() {
 
 // Internal Search 
 // Declarating variables
-barSearch = document.getElementById("barSearch");
-inputSearch = document.getElementById("inputSearch");
-boxSearch = document.getElementById("boxSearch");
+// barSearch = document.getElementById("barSearch");
+// inputSearch = document.getElementById("inputSearch");
+// boxSearch = document.getElementById("boxSearch");
 
 // Function to show box search
-document.getElementById("inputSearch").addEventListener("keyup", internalSearch);
-function internalSearch(){
+// document.getElementById("inputSearch").addEventListener("keyup", internalSearch);
+// function internalSearch(){
    
-  filter = inputSearch.value.toUpperCase();
-  li = boxSearch.getElementsByTagName("li");
+//   filter = inputSearch.value.toUpperCase();
+//   li = boxSearch.getElementsByTagName("li");
 
+//   // Navigating in element to filter with 'li'
+//   for (i = 0; i < li.length; i++){
+//     a = li[i].getElementsByTagName("a")[0];
+//     textValue = a.textContent || a.innerText;
+
+//     if(textValue.toUpperCase().indexOf(filter) > -1){
+//       li[i].style.display = "";
+//       boxSearch.style.display = "block";
+
+//       if(inputSearch.value == ""){
+//         boxSearch.style.display = "none";
+//       }
+//     }
+//     else{
+//       li[i].style.display = "none";
+//     }
+//   }
+
+// }
+
+document.querySelectorAll(".siteSearchInput").forEach(function(i) {
+  i.addEventListener("keyup", function(event) { internalSearch(event.target) });
+})
+
+var filter, searchWrapper, boxSearch, li;
+
+function internalSearch(input) {
+  filter = input.value.toUpperCase();
+  searchWrapper = input.closest('.barSearch')
+  boxSearch = searchWrapper.querySelector('#boxSearch')
+  li = boxSearch.getElementsByTagName("li");
   // Navigating in element to filter with 'li'
-  for (i = 0; i < li.length; i++){
+  for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     textValue = a.textContent || a.innerText;
 
-    if(textValue.toUpperCase().indexOf(filter) > -1){
+    if (textValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
       boxSearch.style.display = "block";
 
-      if(inputSearch.value == ""){
+      if(input.value == ""){
         boxSearch.style.display = "none";
       }
     }
-    else{
+
+    else {
       li[i].style.display = "none";
     }
   }
-
 }
