e37f777e35d7a90790ebb9776a120d978177eefd
mechamech <mechamena@gmail.com>
2022-11-02 14:30:22 -0300

    mensaje not found en búsqueda

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -220,68 +220,42 @@ function navbarTogglerBg() {
   }
 }
 
-
-
-// Internal Search 
-// Declarating variables
-// barSearch = document.getElementById("barSearch");
-// inputSearch = document.getElementById("inputSearch");
-// boxSearch = document.getElementById("boxSearch");
-
-// Function to show box search
-// document.getElementById("inputSearch").addEventListener("keyup", internalSearch);
-// function internalSearch(){
-   
-//   filter = inputSearch.value.toUpperCase();
-//   li = boxSearch.getElementsByTagName("li");
-
-//   // Navigating in element to filter with 'li'
-//   for (i = 0; i < li.length; i++){
-//     a = li[i].getElementsByTagName("a")[0];
-//     textValue = a.textContent || a.innerText;
-
-//     if(textValue.toUpperCase().indexOf(filter) > -1){
-//       li[i].style.display = "";
-//       boxSearch.style.display = "block";
-
-//       if(inputSearch.value == ""){
-//         boxSearch.style.display = "none";
-//       }
-//     }
-//     else{
-//       li[i].style.display = "none";
-//     }
-//   }
-
-// }
-
 document.querySelectorAll(".siteSearchInput").forEach(function(i) {
   i.addEventListener("keyup", function(event) { internalSearch(event.target) });
 })
 
-var filter, searchWrapper, boxSearch, li;
+var filter, searchWrapper, boxSearch, li, match;
 
 function internalSearch(input) {
+  match = false;
   filter = input.value.toUpperCase();
   searchWrapper = input.closest('.barSearch')
-  boxSearch = searchWrapper.querySelector('#boxSearch')
-  li = boxSearch.getElementsByTagName("li");
-  // Navigating in element to filter with 'li'
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    textValue = a.textContent || a.innerText;
+  boxSearch = searchWrapper.querySelector('#boxSearch') 
+  results = boxSearch.querySelector('.results');
+  li = results.getElementsByTagName("li");
+  notFoundMsg = boxSearch.querySelector('.notFoundMessage');
+  boxSearch.style.display = "block";
 
-    if (textValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-      boxSearch.style.display = "block";
+  if (input.value == ""){
+    boxSearch.style.display = "none";
+  }
 
-      if(input.value == ""){
-        boxSearch.style.display = "none";
+  else {
+    for (i = 0; i < li.length; i++) {
+      textValue = li[i].querySelector('a').textContent;
+      li[i].style.display = 'none';
+
+      if (textValue.toUpperCase().indexOf(filter) > -1) {
+        match = true
+        li[i].style.display = "";
+        results.style.display = "block";
+        notFoundMsg.style.display = "none";
       }
     }
+  }
 
-    else {
-      li[i].style.display = "none";
-    }
+  if (!match) {
+    results.style.display = "none";
+    notFoundMsg.style.display = "block";
   }
 }
